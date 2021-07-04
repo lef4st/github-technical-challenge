e2e98577c904664c161a10a4e8713bf6de3e0c07
LEANDRO ROLA BELAS PEREIRA <leandrorbpereira@gmail.com>
2021-07-04 12:06:32 -0300

    Tests added
    Tests added

--- src/main/java/br/trustly/challenge/api/utils/ParserUtils.java
+++ src/main/java/br/trustly/challenge/api/utils/ParserUtils.java
@@ -2,8 +2,6 @@ package br.trustly.challenge.api.utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 
 /**
  * Utility class focused on subjects involving parse
--- src/test/java/br/trustly/challenge/ApiTests.java
+++ src/test/java/br/trustly/challenge/ApiTests.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 /**
  * Test class responsible for testing the API endpoints, 
  * both in success and error cases
@@ -20,6 +19,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest(classes = ChallengeApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiTests extends BaseApiTest {
 
+	public static final String URL_BASE_SUFFIX = "?repoUrl=";
+	
 	@Test
 	public void getIndexPage() {
 		
@@ -36,4 +37,55 @@ class ApiTests extends BaseApiTest {
 		assertEquals("API for scraping a github repository", title);
 	}
 
+	@Test
+	public void wrongUrl() {
+		
+		String wrongUrl = "https://www.bing.com";
+		String link = "/scrapRepoByUrl" + URL_BASE_SUFFIX + wrongUrl;
+		
+		/* Call the API */
+		ResponseEntity response = get(link, String.class);
+		
+		/* Verify returning HTTP code */
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+	}
+	
+	@Test
+	public void defectiveUrl() {
+		
+		String defectiveUrl = "hps://github.com/lef4st/JAVA-MS-Compra-Transacao";
+		String link = "/scrapRepoByUrl" + URL_BASE_SUFFIX + defectiveUrl;
+		
+		/* Call the API */
+		ResponseEntity response = get(link, String.class);
+		
+		/* Verify returning HTTP code */
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+	}
+	
+	@Test
+	public void notFoundUrl() {
+		
+		String notFoundUrl = "https://github.com/lef4st/JAVA-MS";
+		String link = "/scrapRepoByUrl" + URL_BASE_SUFFIX + notFoundUrl;
+		
+		/* Call the API */
+		ResponseEntity response = get(link, String.class);
+		
+		/* Verify returning HTTP code */
+		assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+	}
+	
+	@Test
+	public void correctUrl() {
+		
+		String correctUrl = "https://github.com/lef4st/Weather-Forecast-SPA";
+		String link = "/scrapRepoByUrl" + URL_BASE_SUFFIX + correctUrl;
+		
+		/* Call the API */
+		ResponseEntity response = get(link, String.class);
+		
+		/* Verify returning HTTP code */
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
 }
